876d6b333abe116fc2aab4c1fed5945d3c6f9bb2
alex <alex@slab.org>
2021-07-21 22:37:16 +0100

    reorder and add some documentation

--- Pattern.hs
+++ Pattern.hs
@@ -12,30 +12,36 @@ import qualified Data.Map.Strict as Map
 
 import Prelude hiding ((<*), (*>))
 
+-- | Timespan (called an arc in 'real' tidal)
 data Span = Span {begin :: Rational, end :: Rational}
   deriving (Show)
 
+-- | An event - a value, its 'whole' timespan, and the timespan that
+-- its active (called a 'part' in real tidal)
 data Event a = Event {whole :: Maybe Span,
                       active :: Span, value :: a
                      }
   deriving (Show, Functor)
 
+-- | A pattern - a function from a timespan to a list of events active
+-- during that timespan
 data Pattern a = Pattern {query :: Span -> [Event a]}
   deriving (Functor)
 
 instance Show a => Show (Pattern a) where
   show pat = show $ query pat (Span 0 1)
 
+-- | A control pattern as a map from strings to values
+type ControlPattern = Pattern (Map.Map String Value)
+
+-- | The possible types of values that can be in a control pattern
 data Value = S String
            | F Double
            | R Rational
        deriving (Show)
 
-type ControlPattern = Pattern (Map.Map String Value)
-
-toPolar :: Num a => Pattern a -> Pattern a
-toPolar pat = fmap (\v -> (v*2)-1) pat
-
+-- | A continuous pattern as a function from time to values. Takes the
+-- midpoint of the given query as the time value.
 signal :: (Rational -> a) -> Pattern a
 signal timeF = Pattern {query = f}
   where f (Span b e) = [Event {whole = Nothing,
@@ -44,37 +50,48 @@ signal timeF = Pattern {query = f}
                               }
                        ]
 
+-- | Converts from a range from 0 to 1, to a range from -1 to 1
+toPolar :: Num a => Pattern a -> Pattern a
+toPolar pat = fmap (\v -> (v*2)-1) pat
+
+-- | Sawtooth signal
 saw :: Pattern Rational
 saw = signal (\t -> mod' t 1)
 
 saw2 :: Pattern Rational
 saw2 = toPolar saw
 
+-- | A continuous value
 steady :: a -> Pattern a
 steady v = signal (const v)
 
 silence :: Pattern a
 silence = Pattern (\_ -> [])
 
+-- | The start of the cycle that a given time value is in
 sam :: Rational -> Rational
 sam s = toRational $ floor s
 
+-- | The start of the next cycle
 nextSam :: Rational -> Rational
 nextSam s = sam s + 1
 
+-- | Splits a timespan at cycle boundaries
 spanCycles :: Span -> [Span]
 spanCycles (Span b e) | e <= b = []
                       | sam b == sam e = [Span b e]
                       | otherwise
   = (Span b (nextSam b)):(spanCycles (Span (nextSam b) e))
 
--- | repeat discrete value once per cycle
+-- | Repeat discrete value once per cycle
 atom :: a -> Pattern a
 atom v = Pattern f
   where f s = map (\s' -> Event (Just $ wholeCycle $ begin s') s' v) (spanCycles s)
         wholeCycle :: Rational -> Span
         wholeCycle t = Span (sam t) (nextSam t)
 
+-- | Concatenate a list of patterns (works a little differently from
+-- 'real' tidal, needs some work)
 slowcat :: [Pattern a] -> Pattern a
 slowcat pats = Pattern f
   where f s = concatMap queryCycle $ spanCycles s
@@ -112,19 +129,19 @@ slowappend a b = slowcat [a,b]
 stack :: [Pattern a] -> Pattern a
 stack pats = Pattern $ \s -> concatMap (\pat -> query pat s) pats
 
+-- | Intersection of two timespans, returns Nothing if they don't intersect
 sect :: Span -> Span -> Maybe Span
 sect a b = check $ sect' a b
   where check :: Span -> Maybe Span
         check (Span a b) | b <= a = Nothing
                          | otherwise = Just (Span a b)
 
+-- | Intersection of two timespans
 sect' :: Span -> Span -> Span
 sect' (Span b e) (Span b' e') = Span (max b b') (min e e')
 
-instance Applicative Pattern where
-  pure = steady
-  (<*>) = app sect'
-
+-- | Apply a pattern of values to a pattern of functions, given a
+-- function to merge the 'whole' timespans
 app :: (Span -> Span -> Span) -> Pattern (a -> b) -> Pattern a -> Pattern b
 app wf patf patv = Pattern f
     where f s = concatMap (\ef -> catMaybes $ map (apply ef) evs) efs
@@ -132,11 +149,20 @@ app wf patf patv = Pattern f
                   evs = (query patv s)
                   apply ef ev = apply' ef ev (sect (active ef) (active ev))
                   apply' ef ev Nothing = Nothing
+                  -- TODO - what if one whole is Just, and the other is Nothing ?
                   apply' ef ev (Just s') = Just $ Event (wf <$> (whole ef) <*> (whole ev)) s' (value ef $ value ev)
 
+instance Applicative Pattern where
+  pure = steady
+  (<*>) = app sect'
+
+-- | Alternative definition of <*>, which takes the wholes from the
+-- pattern of functions (unrelated to the <* in Prelude)
 (<*) :: Pattern (a -> b) -> Pattern a -> Pattern b
 (<*) = app const
 
+-- | Alternative definition of <*>, which takes the wholes from the
+-- pattern of values (unrelated to the *> in Prelude)
 (*>) :: Pattern (a -> b) -> Pattern a -> Pattern b
 (*>) = app (flip const)
 
