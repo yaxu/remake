6a8c0416344808e8bdac6760c34c0004311fda99
alex <alex@slab.org>
2021-08-23 21:44:58 +0100

    a test

--- test/Test.hs
+++ test/Test.hs
@@ -11,7 +11,9 @@ import Sound.Tidal2.Parse
 
 main :: IO ()
 main = hspec $ do
-   describe "parse" $ do
-     it "can parse addition $" $
+   describe "Integers" $ do
+     it "can parse a pure number as a pattern" $
        parse (pClosed (T_Pattern T_Int)) "" "(pure 3)" `shouldParse` (Tk_App Tk_pure (Tk_Int 3))
-
+     it "can parse addition" $
+       parse (pClosed (T_Int)) "" "(3 + 3)" `shouldParse` (Tk_app (Tk_App Tk_plus (Tk_Int 3)) (Tk_Int 3))
+     
