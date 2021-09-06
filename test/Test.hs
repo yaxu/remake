3771ad5e8abba7ecb36156b17e74369b5f384051
alex <alex@slab.org>
2021-09-06 08:46:07 +0100

    tests

--- test/Test.hs
+++ test/Test.hs
@@ -15,5 +15,14 @@ main = hspec $ do
      it "can parse a pure number as a pattern" $
        parse (pClosed (T_Pattern T_Int)) "" "(pure 3)" `shouldParse` (Tk_App Tk_pure (Tk_Int 3))
      it "can parse addition" $
-       parse (pClosed (T_Int)) "" "(3 + 3)" `shouldParse` (Tk_app (Tk_App Tk_plus (Tk_Int 3)) (Tk_Int 3))
+       parse (pClosed (T_Int)) "" "(4 + 3)" `shouldParse` (Tk_Op (Just $ Tk_Int 4) Tk_plus (Just $ Tk_Int 3))
+     it "can parse subtraction" $
+       parse (pClosed (T_Int)) "" "(4 - 3)" `shouldParse` (Tk_Op (Just $ Tk_Int 4) Tk_subtract (Just $ Tk_Int 3))
+     it "can parse multiplication" $
+       parse (pClosed (T_Int)) "" "(4 * 3)" `shouldParse` (Tk_Op (Just $ Tk_Int 4) Tk_multiply (Just $ Tk_Int 3))
+     it "can parse division" $
+       parse (pClosed (T_Int)) "" "(4 / 3)" `shouldParse` (Tk_Op (Just $ Tk_Int 4) Tk_divide (Just $ Tk_Int 3))
+     it "can parse arithmetic in parens" $
+       parse (pClosed (T_Int)) "" "(((4 / 3) * 2) + (1+0))" `shouldParse` (Tk_Op (Just (Tk_Op (Just (Tk_Op (Just (Tk_Int 4)) Tk_divide (Just (Tk_Int 3)))) Tk_multiply (Just (Tk_Int 2)))) Tk_plus (Just (Tk_Op (Just (Tk_Int 1)) Tk_plus (Just (Tk_Int 0)))))
+     
      
